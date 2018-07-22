acda8715fef2301c8005c3169abd26974e2171a3
D0zingcat <d0zingcat@outlook.com>
2018-07-22 13:04:25 +0800

    p1s1 added.

---
+++ solutions/1. Two Sum/go/twosum.go
@@ -0,0 +1,13 @@
+package probelm0001
+
+func twoSumS1(nums []int, target int) []int {
+	for i, _ := range nums {
+		for j := i; j < len(nums); j++ {
+			if (nums[i] + nums[j] == target) {
+				return []int{i, j}
+			}
+		}
+	}
+	return nil
+}
+
---
+++ solutions/1. Two Sum/go/twosum_test.go
@@ -0,0 +1,11 @@
+package probelm0001
+
+import (
+	"testing"
+	"fmt"
+)
+
+func TestTwoSumS1(t *testing.T) {
+	nums := []int{2, 7, 11, 15}
+	fmt.Println(twoSumS1(nums, 9))
+}
