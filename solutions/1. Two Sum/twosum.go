45cfa2ec177c30cef12aadbef7ba524742bac805
D0zingcat <d0zingcat@outlook.com>
2018-07-22 23:09:22 +0800

    add another 3 solutions

--- solutions/1. Two Sum/twosum.go
+++ solutions/1. Two Sum/twosum.go
@@ -1,4 +1,5 @@
-package ___Two_Sum
+package problem0001
+
 
 func twoSumS1(nums []int, target int) []int {
 	for i, _ := range nums {
@@ -11,3 +12,39 @@ func twoSumS1(nums []int, target int) []int {
 	return nil
 }
 
+func twoSumS2(nums []int, target int) []int {
+	m := make(map[int](int), len(nums))
+	for i, v := range nums {
+		m[v] = i+1
+	}
+	for i, v := range nums {
+		if (m[target-v] != 0 && i != m[target-v]-1) {
+			return []int{i, m[target-v]-1}
+		}
+	}
+	return nil
+}
+
+
+func twoSumS3(nums []int, target int) []int {
+	m := make(map[int](int), len(nums))
+	for i, v := range nums {
+		if (m[target-v] != 0 && i != m[target-v]-1) {
+			return []int{i, m[target-v]-1}
+		}
+		m[v] = i+1
+	}
+	return nil
+}
+
+func twoSumS4(nums []int, target int) []int {
+	buff := make(map[int](int), len(nums))
+	for i, v := range nums {
+		val, ok := buff[target - v]
+		if ok {
+			return []int{i, val}
+		}
+		buff[v] = i
+	}
+	return nil
+}
--- solutions/1. Two Sum/twosum_test.go
+++ solutions/1. Two Sum/twosum_test.go
@@ -1,4 +1,4 @@
-package ___Two_Sum
+package problem0001
 
 import (
 	"testing"
@@ -8,4 +8,17 @@ import (
 func TestTwoSumS1(t *testing.T) {
 	nums := []int{2, 7, 11, 15}
 	fmt.Println(twoSumS1(nums, 9))
+}
+func TestTwoSumS2(t *testing.T) {
+	nums := []int{3, 3}
+	fmt.Println(twoSumS2(nums, 6))
+}
+
+func TestTwoSumS3(t *testing.T) {
+	nums := []int{3, 3}
+	fmt.Println(twoSumS2(nums, 6))
+}
+func TestTwoSumS4(t *testing.T) {
+	nums := []int{3, 3}
+	fmt.Println(twoSumS2(nums, 6))
 }
